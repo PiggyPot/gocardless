75901b8b29243fc5d1f4de5cc5b26eed40c19269
Phil McClure <phil.mcclure@gmail.com>
2017-06-22 15:29:46 +0100

    Initial commit

---
+++ lib/gocardless.ex
@@ -0,0 +1,18 @@
+defmodule Gocardless do
+  @moduledoc """
+  Documentation for Gocardless.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Gocardless.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/gocardless_test.exs
@@ -0,0 +1,8 @@
+defmodule GocardlessTest do
+  use ExUnit.Case
+  doctest Gocardless
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
