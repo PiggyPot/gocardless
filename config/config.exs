b73014cd6e8540be87af1af3d6e467f5af2e3b7a
Phil McClure <phil.mcclure@gmail.com>
2017-06-23 16:53:17 +0100

    Add default config

--- config/config.exs
+++ config/config.exs
@@ -2,6 +2,10 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
+config :gocardless,
+  api_base: "https://api-sandbox.gocardless.com/",
+  api_version: "2015-07-06",
+  
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
 # file won't be loaded nor affect the parent project. For this reason,
@@ -27,4 +31,4 @@ use Mix.Config
 # Configuration from the imported file will override the ones defined
 # here (which is why it is important to import them last).
 #
-import_config "#{Mix.env}.exs"
+# import_config "#{Mix.env}.exs"
